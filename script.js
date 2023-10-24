e735118378722eeb08f62fa9fe64f61ecc104731
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-24 16:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
+    const form = document.getElementById("form1");
+	const first = form.fname.value;
+    const last = form.lname.value;
+	alert(first + " " + last);
+
 
 }
+
